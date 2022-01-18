f0e1c244b6711bb1c0c0e50e71b38cb6458c1886
MAKE <2975609919@qq.com>
2022-01-18 15:25:58 +0800

    更改

--- 代码/测试.py
+++ 代码/测试.py
@@ -1,3 +1,24 @@
-import re
+def in_weather():
+    temperature = input('请输入今天气温（单位 摄氏度）:')
+    air_pressure = input('请输入今天气压（单位 帕）:')
+    return temperature, air_pressure
 
-re.search()
+
+def weather(temperature, air_pressure):
+    if 25 < temperature <= 30 and 200 < air_pressure <= 300:
+        print('比较舒适')
+    elif 10 < temperature <= 25 and 100 < air_pressure <= 200:
+        print('特别舒适')
+    elif -8 <= temperature <= 10 and 20 <= air_pressure <= 100:
+        print('比较舒适')
+    elif 30 < temperature or temperature < -8:
+        print('不舒适')
+    elif 300 < air_pressure or air_pressure < 20:
+        print('不舒适')
+    else:
+        print('本程序不能判断')
+    return
+
+
+data = in_weather()
+weather(int(data[0]), int(data[1]))
